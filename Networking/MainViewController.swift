b48a2f366b22337ab171980e8052e888a30eeb3a
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2020-03-06 17:38:46 +0300

    Simple interface Compiled

---
+++ Networking/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  Networking
+//
+//  Created by Lucky on 06/03/2020.
+//  Copyright © 2020 DmitriyYatsyuk. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    @IBAction func getRequest(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func postRequest(_ sender: UIButton) {
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -10,22 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var getImageButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         activityIndicator.isHidden = true
         activityIndicator.hidesWhenStopped = true
+        fetchImage()
     }
-    @IBAction func getImagePressed(_ sender: UIButton) {
-        
-        label.isHidden = true
-        getImageButton.isEnabled = false
+    func fetchImage() {
+
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
         
